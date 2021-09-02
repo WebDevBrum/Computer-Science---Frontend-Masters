16fa4a0be1133f4e8e9ea15b7f6fefb7ac77af8b
Scott Davies <scottdavies1983@gmail.com>
2021-09-02 14:19:45 +0100

    completed challenge 14

--- JavaScript the Hard parts V2/Callbacks-and-Higher-Order-Functions.js
+++ JavaScript the Hard parts V2/Callbacks-and-Higher-Order-Functions.js
@@ -240,25 +240,43 @@ function prioritize(array, callback) {
 }
 
 /** * Uncomment these to check your work! ** */
-const startsWithS = function (str) {
-  return str[0] === "s" || str[0] === "S";
-};
-console.log(
-  prioritize(
-    ["curb", "rickandmorty", "seinfeld", "sunny", "friends"],
-    startsWithS
-  )
-); // should log:
-["seinfeld", "sunny", "curb", "rickandmorty", "friends"];
+// const startsWithS = function (str) {
+//   return str[0] === "s" || str[0] === "S";
+// };
+// console.log(
+//   prioritize(
+//     ["curb", "rickandmorty", "seinfeld", "sunny", "friends"],
+//     startsWithS
+//   )
+// ); // should log:
+// ["seinfeld", "sunny", "curb", "rickandmorty", "friends"];
 
 // Challenge 14
-function countBy(array, callback) {}
+// Create a function countBy that accepts an array and a callback, and returns an object. countBy will iterate through the array and perform the callback on each element. Each return value from the callback will be saved as a key on the object. The value associated with each key will be the number of times that particular return value was returned.
 
-// /*** Uncomment these to check your work! ***/
-// console.log(countBy([1, 2, 3, 4, 5], function(num) {
-// if (num % 2 === 0) return 'even';
-// else return 'odd';
-// })); // should log: { odd: 3, even: 2 }
+function countBy(array, callback) {
+  const newObject = {};
+
+  forEach(array, (element) => {
+    const elementToAdd = callback(element);
+
+    if (!(elementToAdd in newObject)) {
+      newObject[elementToAdd] = 1;
+    } else {
+      newObject[elementToAdd] += 1;
+    }
+  });
+
+  return newObject;
+}
+
+/** * Uncomment these to check your work! ** */
+console.log(
+  countBy([1, 2, 3, 4, 5], function (num) {
+    if (num % 2 === 0) return "even";
+    return "odd";
+  })
+); // should log: { odd: 3, even: 2 }
 
 // Challenge 15
 function groupBy(array, callback) {}
