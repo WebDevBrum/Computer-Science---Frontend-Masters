06d8f84d134671dc010ca63e63cf71de492ce90e
Scott Davies <scottdavies1983@gmail.com>
2021-09-08 13:20:45 +0100

    completed challenge 20

--- JavaScript the Hard parts V2/Callbacks-and-Higher-Order-Functions.js
+++ JavaScript the Hard parts V2/Callbacks-and-Higher-Order-Functions.js
@@ -400,14 +400,24 @@ function rating(arrOfFuncs, value) {
 // console.log(rating(checks, 66)); // should log: 75
 
 // Challenge 20
-function pipe(arrOfFuncs, value) {}
+// Create a function pipe that accepts an array (of functions) and a value. pipe should input the value into the first function in the array, and then use the output from that function as input for the second function, and then use the output from that function as input for the third function, and so forth, until we have an output from the last function in the array. pipe should return the final output.
 
-// /*** Uncomment these to check your work! ***/
-// const capitalize = str => str.toUpperCase();
-// const addLowerCase = str => str + str.toLowerCase();
-// const repeat = str => str + str;
+function pipe(arrOfFuncs, value) {
+  let pipedOutput = value;
+
+  arrOfFuncs.forEach((func) => {
+    pipedOutput = func(pipedOutput);
+  });
+
+  return pipedOutput;
+}
+
+/** * Uncomment these to check your work! ** */
+// const capitalize = (str) => str.toUpperCase();
+// const addLowerCase = (str) => str + str.toLowerCase();
+// const repeat = (str) => str + str;
 // const capAddlowRepeat = [capitalize, addLowerCase, repeat];
-// console.log(pipe(capAddlowRepeat, 'cat')); // should log: 'CATcatCATcat'
+// console.log(pipe(capAddlowRepeat, "cat")); // should log: 'CATcatCATcat'
 
 // Challenge 21
 function highestFunc(objOfFuncs, subject) {}
