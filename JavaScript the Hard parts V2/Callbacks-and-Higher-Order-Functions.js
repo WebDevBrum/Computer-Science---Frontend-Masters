3a0770fb0b7311dc9a659f8eaf3d490875fd1a95
Scott Davies <scottdavies1983@gmail.com>
2021-09-08 13:33:52 +0100

    completed challenge 22

--- JavaScript the Hard parts V2/Callbacks-and-Higher-Order-Functions.js
+++ JavaScript the Hard parts V2/Callbacks-and-Higher-Order-Functions.js
@@ -420,13 +420,30 @@ function pipe(arrOfFuncs, value) {
 // console.log(pipe(capAddlowRepeat, "cat")); // should log: 'CATcatCATcat'
 
 // Challenge 21
-function highestFunc(objOfFuncs, subject) {}
+// Create a function highestFunc that accepts an object (which will contain functions) and a subject (which is any value). highestFunc should return the key of the object whose associated value (which will be a function) returns the largest number, when the subject is given as input.
 
-// /*** Uncomment these to check your work! ***/
+function highestFunc(objOfFuncs, subject) {
+  const keys = Object.keys(objOfFuncs);
+  let highest = null;
+  let champion;
+
+  forEach(keys, (key) => {
+    const check = objOfFuncs[key](subject);
+
+    if (check > highest) {
+      highest = check;
+      champion = key;
+    }
+  });
+
+  return champion;
+}
+
+/** * Uncomment these to check your work! ** */
 // const groupOfFuncs = {};
-// groupOfFuncs.double = n => n * 2;
-// groupOfFuncs.addTen = n => n + 10;
-// groupOfFuncs.inverse = n => n * -1;
+// groupOfFuncs.double = (n) => n * 2;
+// groupOfFuncs.addTen = (n) => n + 10;
+// groupOfFuncs.inverse = (n) => n * -1;
 // console.log(highestFunc(groupOfFuncs, 5)); // should log: 'addTen'
 // console.log(highestFunc(groupOfFuncs, 11)); // should log: 'double'
 // console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
