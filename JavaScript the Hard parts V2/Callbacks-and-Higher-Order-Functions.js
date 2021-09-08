1dc14c593a7eb2a7d007ec7816d3c3a9ffe3b750
Scott Davies <scottdavies1983@gmail.com>
2021-09-08 13:38:32 +0100

    completed challenge 22

--- JavaScript the Hard parts V2/Callbacks-and-Higher-Order-Functions.js
+++ JavaScript the Hard parts V2/Callbacks-and-Higher-Order-Functions.js
@@ -449,7 +449,17 @@ function highestFunc(objOfFuncs, subject) {
 // console.log(highestFunc(groupOfFuncs, -20)); // should log: 'inverse'
 
 // Challenge 22
-function combineOperations(startVal, arrOfFuncs) {}
+// Create a function, combineOperations, that takes two parameters: a starting value and an array of functions. combineOperations should pass the starting value into the first function in the array. combineOperations should pass the value returned by the first function into the second function, and so on until every function in the array has been called. combineOperations should return the final value returned by the last function in the array.
+
+function combineOperations(startVal, arrOfFuncs) {
+  let pipedOutput = startVal;
+
+  arrOfFuncs.forEach((func) => {
+    pipedOutput = func(pipedOutput);
+  });
+
+  return pipedOutput;
+}
 
 function add100(num) {
   return num + 100;
@@ -463,9 +473,17 @@ function multiplyByThree(num) {
   return num * 3;
 }
 
-// /*** Uncomment these to check your work! ***/
-// console.log(combineOperations(0, [add100, divByFive, multiplyByThree])); // Should output 60 -->
-// console.log(combineOperations(0, [divByFive, multiplyFive, addTen])); // Should output 10
+function multiplyFive(num) {
+  return num * 5;
+}
+
+function addTen(num) {
+  return num + 10;
+}
+
+/** * Uncomment these to check your work! ** */
+console.log(combineOperations(0, [add100, divByFive, multiplyByThree])); // Should output 60 -->
+console.log(combineOperations(0, [divByFive, multiplyFive, addTen])); // Should output 10
 
 // Challenge 23
 function myFunc(array, callback) {}
