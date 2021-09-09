d8096495a16fd1725ee904f6a1f44c9f8dce6fc6
Scott Davies <scottdavies1983@gmail.com>
2021-09-09 14:13:58 +0100

    completed closure challenges 1-4

--- JavaScript the Hard parts V2/Closure-Challenges.js
+++ JavaScript the Hard parts V2/Closure-Challenges.js
@@ -3,22 +3,37 @@
 // Challenge questions can be found at csbin.io/closures
 
 // CHALLENGE 1
-function createFunction() {}
+// Create a function createFunction that creates and returns a function. When that created function is called, it should print "hello". When you think you completed createFunction, un-comment out those lines in the code and run it to see if it works.
+function createFunction() {
+  function innerFunction() {
+    console.log("hello");
+  }
+  return innerFunction;
+}
 
-// /*** Uncomment these to check your work! ***/
+// /** * Uncomment these to check your work! ** */
 // const function1 = createFunction();
 // function1(); // => should console.log('hello');
 
 // CHALLENGE 2
-function createFunctionPrinter(input) {}
+// Create a function createFunctionPrinter that accepts one input and returns a function. When that created function is called, it should print out the input that was used when the function was created.
+function createFunctionPrinter(input) {
+  function innerFunction() {
+    console.log(input);
+  }
+  return innerFunction;
+}
 
-// /*** Uncomment these to check your work! ***/
-// const printSample = createFunctionPrinter('sample');
+// /** * Uncomment these to check your work! ** */
+// const printSample = createFunctionPrinter("sample");
 // printSample(); // => should console.log('sample');
-// const printHello = createFunctionPrinter('hello');
+// const printHello = createFunctionPrinter("hello");
 // printHello(); // => should console.log('hello');
 
 // CHALLENGE 3
+// Examine the code for the outer function. Notice that we are returning a function and that function is using variables that are outside of its scope.
+// Uncomment those lines of code. Try to deduce the output before executing. Now we are going to create a function addByX that returns a function that will add an input by x.
+
 function outer() {
   let counter = 0; // this variable is outside incrementCounter's scope
   function incrementCounter() {
@@ -28,13 +43,13 @@ function outer() {
   return incrementCounter;
 }
 
-const willCounter = outer();
-const jasCounter = outer();
+// const willCounter = outer();
+// const jasCounter = outer();
 
 // Uncomment each of these lines one by one.
 // Before your do, guess what will be logged from each function call.
 
-// /*** Uncomment these to check your work! ***/
+/** * Uncomment these to check your work! ** */
 // willCounter();
 // willCounter();
 // willCounter();
@@ -42,10 +57,18 @@ const jasCounter = outer();
 // jasCounter();
 // willCounter();
 
-function addByX(x) {}
+function addByX(x) {
+  // this variable is outside incrementCounter's scope
+  function incrementCounter(input) {
+    // counter += x;
+    console.log("counter", input + x);
+    return input + x;
+  }
+  return incrementCounter;
+}
 
 // /*** Uncomment these to check your work! ***/
-// const addByTwo = addByX(2);
+const addByTwo = addByX(2);
 // addByTwo(1); // => should return 3
 // addByTwo(2); // => should return 4
 // addByTwo(3); // => should return 5
@@ -59,13 +82,29 @@ function addByX(x) {}
 // addByFour(5); // => should return 9
 
 // CHALLENGE 4
-function once(func) {}
+// Write a function once that accepts a callback as input and returns a function. When the returned function is called the first time, it should call the callback and return that output. If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called.
+function once(func) {
+  let numberOfCalls = 0;
+  let firstOutput = null;
+
+  function innerFunction(input) {
+    if (numberOfCalls < 1) {
+      firstOutput = func(input);
+      numberOfCalls += 1;
+      return firstOutput;
+    }
+    numberOfCalls += 1;
+    return firstOutput;
+  }
+
+  return innerFunction;
+}
 
 // /*** Uncomment these to check your work! ***/
 // const onceFunc = once(addByTwo);
-// console.log(onceFunc(4));  // => should log 6
-// console.log(onceFunc(10));  // => should log 6
-// console.log(onceFunc(9001));  // => should log 6
+// console.log(onceFunc(4)); // => should log 6
+// console.log(onceFunc(10)); // => should log 6
+// console.log(onceFunc(9001)); // => should log 6
 
 // CHALLENGE 5
 function after(count, func) {}
