8fd1a89832ba7ae77107361c253adac56e5bdf3e
Scott Davies <scottdavies1983@gmail.com>
2021-09-13 15:39:55 +0100

    completed challenge 8

--- JavaScript the Hard parts V2/Closure-Challenges.js
+++ JavaScript the Hard parts V2/Closure-Challenges.js
@@ -161,21 +161,38 @@ function rollCall(names) {
 }
 
 /** * Uncomment these to check your work! ** */
-const rollCaller = rollCall(["Victoria", "Juan", "Ruth"]);
-rollCaller(); // => should log 'Victoria'
-rollCaller(); // => should log 'Juan'
-rollCaller(); // => should log 'Ruth'
-rollCaller(); // => should log 'Everyone accounted for'
+// const rollCaller = rollCall(["Victoria", "Juan", "Ruth"]);
+// rollCaller(); // => should log 'Victoria'
+// rollCaller(); // => should log 'Juan'
+// rollCaller(); // => should log 'Ruth'
+// rollCaller(); // => should log 'Everyone accounted for'
 
 // CHALLENGE 8
-function saveOutput(func, magicWord) {}
+// Create a function saveOutput that accepts a function (that will accept one argument), and a string (that will act as a password). saveOutput will then return a function that behaves exactly like the passed-in function, except for when the password string is passed in as an argument. When this happens, the returned function will return an object with all previously passed-in arguments as keys, and the corresponding outputs as values.
 
-// /*** Uncomment these to check your work! ***/
-// const multiplyBy2 = function(num) { return num * 2; };
-// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-// console.log(multBy2AndLog(2)); // => should log 4
-// console.log(multBy2AndLog(9)); // => should log 18
-// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+function saveOutput(func, magicWord) {
+  const inOutObject = {};
+
+  function outputRunner(password) {
+    if (password === magicWord) {
+      console.log(inOutObject);
+    } else {
+      if (!inOutObject[password]) inOutObject[password] = func(password);
+
+      return func(password);
+    }
+  }
+  return outputRunner;
+}
+
+/** * Uncomment these to check your work! ** */
+const multiplyBy2 = function (num) {
+  return num * 2;
+};
+const multBy2AndLog = saveOutput(multiplyBy2, "boo");
+console.log(multBy2AndLog(2)); // => should log 4
+console.log(multBy2AndLog(9)); // => should log 18
+console.log(multBy2AndLog("boo")); // => should log { 2: 4, 9: 18 }
 
 // CHALLENGE 9
 function cycleIterator(array) {}
