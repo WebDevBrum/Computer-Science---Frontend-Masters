6890617e06b7426891d096b69b02293fe6a075ce
Scott Davies <scottdavies1983@gmail.com>
2021-09-13 13:47:48 +0100

    completed challenge 5

--- JavaScript the Hard parts V2/Closure-Challenges.js
+++ JavaScript the Hard parts V2/Closure-Challenges.js
@@ -107,14 +107,27 @@ function once(func) {
 // console.log(onceFunc(9001)); // => should log 6
 
 // CHALLENGE 5
-function after(count, func) {}
+// Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter
+function after(count, func) {
+  let counter = 0;
 
-// /*** Uncomment these to check your work! ***/
-// const called = function() { console.log('hello') };
-// const afterCalled = after(3, called);
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => 'hello' is printed
+  function innerFunction() {
+    counter += 1;
+    if (counter === count) {
+      func();
+    }
+  }
+  return innerFunction;
+}
+
+/** * Uncomment these to check your work! ** */
+const called = function () {
+  console.log("hello");
+};
+const afterCalled = after(3, called);
+afterCalled(); // => nothing is printed
+afterCalled(); // => nothing is printed
+afterCalled(); // => 'hello' is printed
 
 // CHALLENGE 6
 function delay(func, wait) {}
